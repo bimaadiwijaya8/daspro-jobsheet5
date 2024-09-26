428c58493e259c49ca76f29612e671806ad46bef
bimaadiwijaya <bimaadiwijaya83@gmail.com>
2024-09-26 07:22:01 +0700

    Modifikasi Ternary Operator

--- PemilihanBilangan07.java
+++ PemilihanBilangan07.java
@@ -7,10 +7,14 @@ public class PemilihanBilangan07 {
         System.out.println("Masukkan sebuah angka: ");
         int angka  = sc.nextInt();
 
-        if (angka % 2 == 0) {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        } else {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        // if (angka % 2 == 0) {
+        //     System.out.println("Angka " + angka + " termasuk bilangan genap");
+        // } else {
+        //     System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+        // }
+
+        String perbandingan = (angka % 2 == 0) ? "Angka " + angka + " termasuk bilangan genap" : "Angka " + angka + " termasuk bilangan ganjil";
+        System.out.println(perbandingan);
     }    
+
 }
